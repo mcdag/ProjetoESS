aa980dc61bfc6c06701eb8053b5a2aa71ab630c8
Paulo Matheus <pmssm@cin.ufpe.br>
2021-08-22 12:03:08 -0300

    feat:  criação de cenários de metas

---
+++ tests-acceptance/features/metas.feature
@@ -0,0 +1,3 @@
+Feature: As a professor
+         I want to register, remove and edit students metas
+         So that I can manage their learning goals
---
+++ tests-acceptance/stepdefinitions/metas.ts
@@ -0,0 +1,7 @@
+import { defineSupportCode } from 'cucumber';
+import { browser, $, element, ElementArrayFinder, by } from 'protractor';
+let chai = require('chai').use(require('chai-as-promised'));
+let expect = chai.expect;
+import request = require("request-promise");
+
+var base_url = "http://localhost:3000/";
