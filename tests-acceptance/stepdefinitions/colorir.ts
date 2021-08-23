741b7c23e8f2b6a1d2ceb23964e804ad9f2ee6b7
Paulo Matheus <pmssm@cin.ufpe.br>
2021-08-23 16:21:32 -0300

    feat colorir

--- tests-acceptance/stepdefinitions/colorir.ts
+++ tests-acceptance/stepdefinitions/colorir.ts
@@ -7,47 +7,30 @@ import request = require("request-promise");
 var base_url = "http://localhost:3000/";
 
 let sameCPF = ((elem, cpf) => elem.element(by.name('cpflist')).getText().then(text => text === cpf));
-let sameMeta = ((elem, meta) => elem.element(by.name('metalist')).getText().then(text => text === meta));
-let sameNota = ((elem, nota) => elem.element.getText().then(text => text === nota));
-let sameColor = ((elem, color) => elem.element(by.css('background-color')).getText().then(text => text === color));
-
-let pAND = ((p,q) => p.then(a => q.then(b => a && b)))
+let sameMeta =  ((elem, meta) => elem.getAttribute('value').then(text => text === meta));
 
 async function criarMeta(meta) {
-    await $("input[name='metabox']").sendKeys(<string> meta);
+    await $("input[name='novaMeta']").sendKeys(<string> meta);
     await element(by.buttonText('Adicionar')).click();
 }
 
 async function criarNota(cpf, meta, nota) {
-    var optionNum = 0;
-    if(nota == "MANA"){
-        optionNum = 0;
-    }else if(nota == "MPA"){
-        optionNum = 1;
-    }else{
-        optionNum = 2;
-    }
-    element.all(by.name(cpf+'select'+meta))   
-    .then(function(options){
-      options[optionNum].click();
-    });   
+    element(by.className(cpf+meta+nota)).click();
 }
 
 async function assertTamanhoEqual(set,n) {
     await set.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(n));
 }
 
-
-async function assertElementsWithSameCPFAndMeta(n,cpf,meta) { 
-    var allalunos : ElementArrayFinder = element.all(by.name('alunolist'));
-    var samecpfsandmeta = allalunos.filter(elem => pAND(sameCPF(elem,cpf),sameMeta(elem,meta)));
-    await assertTamanhoEqual(samecpfsandmeta,n);
+async function assertElementsWithSameMeta(n,meta) {
+    var allmetas : ElementArrayFinder = element.all(by.name('inputmeta'));
+    var samemetas = allmetas.filter(elem => sameMeta(elem,meta));
+    await assertTamanhoEqual(samemetas,n); 
 }
 
-async function assertElementsWithSameNotaAndColor(n, nota, color) { 
-    var allmetas : ElementArrayFinder = element.all(by.class('nota'));
-    var samenotasandcolor = allmetas.filter(elem => pAND(sameColor(elem,color),sameNota(elem,nota)));
-    await assertTamanhoEqual(samenotasandcolor,n);
+async function assertElementsWithSameColor(n, meta, cpf, color) { 
+    var elem = element(by.id(cpf+meta))
+    await elem.getCssValue('background-color').then(value => value === color);
 }
 
 async function assertElementsWithSameCPF(n,cpf) {
@@ -62,21 +45,21 @@ defineSupportCode(function ({ Given, When, Then }) {
         await expect(browser.getTitle()).to.eventually.equal('TaGui');
         await $("a[name='metas']").click();
     })
-    
+     
     Given(/^I see a student with CPF "(\d*)" in the students list$/, async (cpf) => {
         await assertElementsWithSameCPF(1,cpf); 
     });
 
     Given(/^I see the student with CPF "(\d*)" has meta "([^\"]*)"$/, async (cpf, meta) => {
         await criarMeta(meta);
-        await assertElementsWithSameCPFAndMeta(1,cpf, meta); 
+        await assertElementsWithSameMeta(1, meta); 
     });
     When(/^I select "([^\"]*)" to "([^\"]*)" for the student with CPF "(\d*)"$/, async (nota, meta, cpf) => {
         await criarNota(cpf, meta, nota);       
     });
-    
-    Then(/^I can see "([^\"]*)" background in "([^\"]*)"$/, async (nota, color) => {
-        await assertElementsWithSameNotaAndColor(1,nota, color);
+
+    Then(/^I can see the grade to "([^\"]*)" for the student with CPF "(\d*)" background in "([^\"]*)"$/, async (meta, cpf, color) => {
+        await assertElementsWithSameColor(1,meta, cpf, color);
     });
     
 })
